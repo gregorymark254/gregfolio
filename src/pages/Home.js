b0d176faae6a37c3b2689f7c8231dd6d6132ebc0
gregorymark254 <98938152+gregorymark254@users.noreply.github.com>
2022-11-12 10:34:43 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
     this.tick();
     this.isDeleting = false;
   };
-    
+
   TxtType.prototype.tick = function() {
     var i = this.loopNum % this.toRotate.length;
     var fullTxt = this.toRotate[i];
@@ -36,25 +36,24 @@ const Home = () => {
     this.isDeleting = false;
     this.loopNum++;
     delta = 500;
-  }
+    }
 
-  setTimeout(function() {
+    setTimeout(function() {
     that.tick();
     }, delta);
   };
-    
+
   window.onload = function() {
     var elements = document.getElementsByClassName('typewrite');
     for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-type');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtType(elements[i], JSON.parse(toRotate), period);
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
     }
-  }
-
-  // INJECT CSS
-  var css = document.createElement("style");
+    // INJECT CSS
+    var css = document.createElement("style");
     css.type = "text/css";
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);

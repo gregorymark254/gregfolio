3d1097f457f5458a8f907a4548b0805b9abfbf07
gregorymark254 <98938152+gregorymark254@users.noreply.github.com>
2022-11-12 11:14:11 +0300

    Update Footer.js

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Footer = () => {
   return (
     <main className="bg-[#181818] text-white p-8">
-        <div className="container mx-auto p-4">
-            <div className="grid place-items-center text-justify justify-center space-y-3">
-                <h1  className="text-[#F55353]"><b><u>GM.</u></b></h1>
-                <p>Today I am better than yesterday and tomorrow I will be better than today,  because the discipline beats intelligence!</p>
-                <h4>&copy;2022 By<span className="text-[#F55353]">Gregory Mark.</span></h4>
-            </div>
+      <div className="container mx-auto p-4">
+        <div className="grid place-items-center text-justify justify-center space-y-3">
+          <h1 className="text-[#F55353]"><b><u>GM.</u></b></h1>
+          <p>Today I am better than yesterday and tomorrow I will be better than today,  because the discipline beats intelligence!</p>
+          <h5>&copy; 2022 By <a href="/" className="text-[#F55353]">Gregory Mark.</a></h5>
         </div>
+      </div>
     </main>
   )
 }

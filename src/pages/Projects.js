6b97ce4d3b263ac257c15f2dc9a1160f507e87e5
gregorymark254 <98938152+gregorymark254@users.noreply.github.com>
2022-11-12 14:14:17 +0300

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,7 +13,7 @@ const Projects = () => {
                             <a href="https://royallens.vercel.app" >
                             <div className="content-overlay"></div>
                             <img className="content-image" alt="" src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"/>
-                            <div className="content-details fadeIn-bottom">
+                            <div className="content-details fadeIn-left">
                                 <h3 className="content-title">Royal Lens</h3>
                                 <p className="content-text">A Photography website</p>
                             </div>
@@ -26,7 +26,7 @@ const Projects = () => {
                             <a href="http://mental-hub.vercel.app/" >
                             <div className="content-overlay"></div>
                             <img className="content-image" alt="" src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"/>
-                            <div className="content-details fadeIn-bottom">
+                            <div className="content-details fadeIn-top">
                                 <h3 className="content-title">Mental Hub</h3>
                                 <p className="content-text">A non-profit Organization</p>
                             </div>
@@ -39,7 +39,7 @@ const Projects = () => {
                             <a href="http://gr-help.netlify.app/" >
                             <div className="content-overlay"></div>
                             <img className="content-image" alt="" src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"/>
-                            <div className="content-details fadeIn-bottom">
+                            <div className="content-details fadeIn-right">
                                 <h3 className="content-title">Helping Hands</h3>
                                 <p className="content-text">A community website</p>
                             </div>
@@ -52,7 +52,7 @@ const Projects = () => {
                             <a href="https://carloz.vercel.app/" >
                             <div className="content-overlay"></div>
                             <img className="content-image" alt="" src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"/>
-                            <div className="content-details fadeIn-bottom">
+                            <div className="content-details fadeIn-bottom fadeIn-left">
                                 <h3 className="content-title">JOHN CARLOZ</h3>
                                 <p className="content-text">A Portfolio website</p>
                             </div>

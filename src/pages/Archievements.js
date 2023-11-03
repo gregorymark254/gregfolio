3acf524d45c8dabc97c93e92b03dafeeefdfd59e
gregorymark254 <gregorymark254@gmail.com>
2023-11-03 22:21:36 +0300

    portfolio

--- src/pages/Archievements.js
+++ src/pages/Archievements.js
@@ -6,17 +6,17 @@ const Archievements = () => {
     <div>
       <div className="container mx-auto p-4">
         <h1 className="text-center py-4"><b>My<span className="bg-[#F55353] text-white py-2 px-3 rounded-md ">Archievements</span></b></h1>
-        <div className="flex flex-wrap items-center justify-center p-4">
-          <div>
+        <div className="md:flex justify-center p-4">
+          <div className="md:w-1/2">
             <img width={400} height={400} src={archieve} alt="archievement1" />
           </div>
-          <div className="flex flex-wrap ">
+          <div className="flex flex-wrap gap-4">
             <div>
               <h1>06</h1>
               <p>October</p>
               <p>2023</p>
             </div>
-            <div>
+            <div className="md:w-1/2">
               <h2>Victory at the KPF Hackathon 🏆</h2>
               <p>Attended the kilimani Prop Tech Hackathon! And presented Our project, focused on noise monitoring in nightlife venues, 
                 securing the top spot. Grateful for the opportunity brought forth by KPF to showcase innovations and create positive change.
